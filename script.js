bc5048b00831c7ea65ee57beb7723ea0a7b1b694
Matthew Schmidt <47067179+matts0809@users.noreply.github.com>
2019-04-03 23:10:04 -0500

    Added Responsive to Portfolio, Contact, Header

--- script.js
+++ script.js
@@ -1,58 +1,25 @@
-
-/**
-$(document).ready(function(){
-  $('a[href*="#"]').one('click',function (e) {
-    e.preventDefault();
-
-    var target = this.hash;
-    var $target = $(target);
-
-    //Scroll and show hash
-    $('html, body').animate({
-      'scrollTop': $target.offset().top
-    }, 1000, 'swing', function () {
-      window.location.hash = target;
-    });
-  });
-});
-
-
-$(document).ready(function(){
-  $('.nav-link').click(function(){
-    $('.nav-item .nav-link').removeClass("active");
-    $(this).addClass("active");
-
-  })
-});
-**/
-
 //PARALLAX
 
 $(window).scroll(function() {
   parallax();
-})
+});
 
 function parallax() {
 
   var wScroll = $(window).scrollTop();
 
-  $('.parallax--bg').css('background-position', 'center '+(wScroll*0.5)+'px')
+  $('.parallax--bg').css('background-position', 'center '+(wScroll*0.5)+'px');
 }
-
 
 
 
 $(document).ready(function() {
 
-  var scrollLink = $('.scroll');
-
-  //SMOOTH SCROLLING
-  scrollLink.click(function(event) {
-    event.preventDefault();
-    $('html,body').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000 )
-  })
+  $('.scroll').click(function(e){
+    e.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top});
+});
+});
 
 //ACTIME LINK SWITCHING
 $(window).scroll(function() {
@@ -60,12 +27,11 @@ $(window).scroll(function() {
 
   scrollLink.each(function() {
 
-    var sectionOffset = (this.hash).offset().top
+    var sectionOffset = (this.hash).offset().top;
 
     if (sectionOffset <= scrollbarLocation ) {
       $(this).parent().addClass('active');
     }
-  })
-})
+  });
 
-})
+});
